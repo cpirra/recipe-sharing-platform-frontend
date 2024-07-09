7519feb6c5a73ede0e859fe7e506de6d253cb370
Besarta Kuci <besartakuci@gmail.com>
2024-07-09 13:22:37 +0200

    Layouts

--- src/router/index.js
+++ src/router/index.js
@@ -5,27 +5,37 @@ import HomeView from '../views/HomeView.vue'
 import albumsRoutes from './albums'
 import authorizationRoutes from './authorization'
 import profileRoutes from './profile'
+import DefaultLayout from '../layouts/DefaultLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Public',
+      component: DefaultLayout,
+      redirect: '/',
+      children:[
+        {
+          path: '/',
+          name: 'Home',
+          component: HomeView
+        },
+        {
+          path: '/about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/AboutView.vue'),
+        },
+        // Spread the imported routes
+        albumsRoutes,
+        ...authorizationRoutes,
+        profileRoutes,
+      ]
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    // Spread the imported routes
-    albumsRoutes,
-    ...authorizationRoutes,
-    profileRoutes,
+    
   ]
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <script setup>
-import { ref } from 'vue'
-
-
 
 </script>
 
 <template>
   <main>
+    <div class="home">
     <!-- <TheWelcome /> -->
     <!-- Emits Component
     <Emits :value="value" @customEvent="handleCustomEvent" /> -->
     <h1>This is root component</h1>
+    </div>
   </main>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .home{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
