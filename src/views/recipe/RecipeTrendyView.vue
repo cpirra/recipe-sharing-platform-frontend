5bd4a53bcf7dd018ce87c17869e8d7ab48560d86
besartakuci <besartakuci@gmail.com>
2024-07-28 00:22:34 +0200

    Trendy Recipes component + View + cleaner code with components

---
+++ src/views/recipe/RecipeTrendyView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TrendyRecipes from '@/components/Recipe/TrendyRecipes.vue';
+</script>
+
+<template>
+    <TrendyRecipes :limit="10" :showPagination="true"/>
+</template>
+
+<style>
+</style>
