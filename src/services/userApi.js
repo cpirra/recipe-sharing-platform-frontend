80535ebb3849cb4ccc301e953b67bc7bd971b92d
Drini Idrizi <drin.idrizi@gmail.com>
2024-08-04 19:08:35 +0200

    some .env variable fixes

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://localhost:7036/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
--- src/utils/apollo.js
+++ src/utils/apollo.js
@@ -1,10 +1,9 @@
-// src/utils/apollo.js
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core' // Import gql from core
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client/core';
 
 const client = new ApolloClient({
-  uri: 'https://localhost:7036/graphql/', // Your GraphQL endpoint
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT, // Using environment variable for GraphQL endpoint
   cache: new InMemoryCache()
-})
+});
 
-export default client
-export { gql } // Export gql from here
+export default client;
+export { gql }; // Export gql from here
