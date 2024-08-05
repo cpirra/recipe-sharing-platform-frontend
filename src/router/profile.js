b4ff88841f8d4c811e0191c5289d9c03bd7ceb80
Drini Idrizi <drin.idrizi@gmail.com>
2024-08-05 05:00:54 +0200

    Can watch another profile and follow

--- src/router/profile.js
+++ src/router/profile.js
@@ -1,10 +1,17 @@
 // src/router/profile.js
 import ProfileView from '../views/general/ProfileView.vue';
+import AnotherProfileView from '../views/general/AnotherProfileView.vue'
 
 export default [
   {
     path: '/profile',
     component: ProfileView,
     meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile/:userId',
+    component: AnotherProfileView,
+    meta: { requiresAuth: true },
   }
+  
 ];
